de2e4c48640fed5642727396726d725b99715ff8
Priyank Pulumati <pulumati.priyank@gmail.com>
2023-04-25 20:17:44 +0530

    fix typo in migrations

--- db/migrate/20230423192634_create_books.rb
+++ db/migrate/20230423192634_create_books.rb
@@ -2,7 +2,9 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :name
-      t.text :embeddings
+      t.text :cover
+      t.boolean :is_active, default: true
+      t.string :status, default: 'pending'
       t.timestamps
     end
   end
--- db/migrate/20230424221603_create_book_embeddings.rb
+++ db/migrate/20230424221603_create_book_embeddings.rb
@@ -3,9 +3,6 @@ class CreateBookEmbeddings < ActiveRecord::Migration[7.0]
     create_table :book_embeddings do |t|
       t.text :pages
       t.text :embeddings
-      t.text :cover
-      t.boolean :is_active, default: true
-      t.string :status, default: 'pending'
       t.timestamps
     end
   end
