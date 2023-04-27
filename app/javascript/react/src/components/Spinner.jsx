11c79b2b0f23ba132365470a1f0c0d99c6e8f381
Priyank Pulumati <pulumati.priyank@gmail.com>
2023-04-27 06:08:50 +0530

    fix styling for spinner

--- app/javascript/react/src/components/Spinner.jsx
+++ app/javascript/react/src/components/Spinner.jsx
@@ -7,15 +7,15 @@ const Spinner = (props) => {
     <>
       <div
         style={{ zIndex: 1000000 }}
-        className="absolute inset-0 flex justify-center items-center"
+        className="absolute top-0 inset-0 flex h-screen w-screen justify-center items-center"
       >
         <div className="flex justify-center items-center">
           <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-500"></div>
         </div>
       </div>
       <div
-        style={{ zIndex: 1000000 - 1, width: '100%', height: '100%' }}
-        className="absolute bg-gray-700 bg-opacity-75"
+        style={{ zIndex: 1000000 - 1 }}
+        className="absolute bg-gray-700 bg-opacity-75 top-0 inset-0 flex h-screen w-screen"
       ></div>
     </>
   )
